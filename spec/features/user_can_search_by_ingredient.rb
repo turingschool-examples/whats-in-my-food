a6739387342c78d09ba38c297e8e3287d41d3c11
Grant Dempsey <CryptoDemps@gmail.com>
2020-12-14 09:23:29 -0700

    Create test for user_can_search_by_ingredient.rb

---
+++ spec/features/user_can_search_by_ingredient.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe 'user can search for food by ingredient' do
+  it 'and see the results on the page' do
+
+    visit root_path
+
+    fill_in :search, with: "sweet potatoes"
+
+    click "Search"
+
+    expect(current_path).to eq("/foods")
+
+    expect(page).to have_content("8 Results")
+  end
+end
+
+# As a user,
+# When I visit "/"
+# And I fill in the search form with "sweet potatoes"
+# (Note: Use the existing search form)
+# And I click "Search"
+# Then I should be on page "/foods"
+# Then I should see a total of the number of items returned by the search.
+# Then I should see a list of ten foods that contain the ingredient "sweet potatoes"
+# And for each of the foods I should see:
+# - The food's GTIN/UPC code
+# - The food's description
+# - The food's Brand Owner
+# - The food's ingredients
