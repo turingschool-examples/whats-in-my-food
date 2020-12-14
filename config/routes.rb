0fbca02bb58d2cb1325149e829a6f63498d7526d
George Soderholm <georgesoderholm@gmail.com>
2020-12-14 09:59:53 -0700

    WIP

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root "welcome#index"
+  get '/search', to: 'search#index', as: 'foods'
 end
--- spec/features/user_can_search_spec.rb
+++ spec/features/user_can_search_spec.rb
@@ -6,12 +6,12 @@ RSpec.describe 'As a User' do
       # When I visit "/"
       visit root_path
       # And I fill in the search form with "sweet potatoes"
-      select 'sweet potatoes', from: :q
+      fill_in :search_field, with: 'sweet potatoes'
       # (Note: Use the existing search form)
       # And I click "Search"
       click_button 'Search'
       # Then I should be on page "/foods"
-      expect(current_path).to eq(search_path)
+      expect(current_path).to eq(foods_path)
       # Then I should see a total of the number of items returned by the search.
       expect(page).to have_content('Total number of Items with sweet potatoes: 20')
       # Then I should see a list of ten foods that contain the ingredient "sweet potatoes"
@@ -25,6 +25,7 @@ RSpec.describe 'As a User' do
         expect(page).to have_css('.brand_owner')
       # - The food's ingredients
         expect(page).to have_css('.ingredients')
+      end
     end
   end
 end
