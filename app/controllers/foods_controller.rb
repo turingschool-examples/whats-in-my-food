2b35aff491b0b07931b2ecc87c769208f7c37b3d
Grant Dempsey <CryptoDemps@gmail.com>
2020-12-14 10:40:17 -0700

    Finally got the API hooked up correctly

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,15 +1,15 @@
 class FoodsController < ApplicationController
   def index
-    ingredient = params[:q]
-
-    conn = Faraday.new("https://api.nal.usda.gov/fdc") do |f|
-      f.params["api-key"] = ENV["API_KEY"]
+    query = params[:q]
+    conn = Faraday.new("https://api.nal.usda.gov") do |f|
+      f.params["api_key"] = ENV["API_KEY"]
     end
 
-    response = conn.get("/v1/foods/search?&query=#{ingredient}") do |req|
-      require "pry"
-      binding.pry
+    response = conn.get("/fdc/v1/foods/search") do |req|
+      req.params[:q] = query
     end
+    # https://api.nal.usda.gov/fdc/v1/foods/search?api_key=fl6k6gMJqzehZDs1qP9uq2dDi2gaK9Fj2stPDIUx&query=Cheddar%20Cheese
+
     food_info = JSON.parse(response.body, symbolize_names: true)
   end
 end
