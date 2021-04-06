45d3a54fca59bb7d2044def1ef539fd6bbc264e7
kylejschulz <kylschulz@gmail.com>
2021-04-06 15:24:36 -0400

    add test

---
+++ spec/foods/index_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "as a user" do
+  describe "when i visit the /" do
+    it "fills in sweet potatoes in the form and clicks search" do
+      visit '/'
+
+      expect(current_path).to eq.('/')
+      fill_in 'q', with: 'sweet potatoes'
+      click_on 'Search'
+      expect(current_path).to eq(foods_index_path)
+      expect(page).to have_content('total search results')
+      within('.results') do
+        #expect 10 results
+      end
+
+      within('.results first') do
+        expect(page).to have_content("GTIN/UPC code")
+        expect(page).to have_content("Description")
+        expect(page).to have_content("Brand Owner")
+        expect(page).to have_content("Ingredients")
+      end
+    end
+  end
+end
