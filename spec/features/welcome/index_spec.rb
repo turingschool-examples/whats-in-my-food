db78294230c274864beadffe6b0a5d195a3fe87d
Aidan McGuire <>
2021-05-24 09:03:26 -0600

    Add test for welcome page search form

---
+++ spec/features/welcome/index_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe 'welcome page' do
+  it 'has a search form that redirects to /foods' do
+    visit root_path
+
+    fill_in :q, with: 'banana'
+    click_button 'Search'
+
+    expect(current_path).to eq(foods_path)
+  end
+end
