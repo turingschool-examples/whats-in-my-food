3552137208b08f51520deed09c2f241c05eaac93
joemecha <jwmecha@gmail.com>
2021-05-24 11:55:32 -0500

    Fixed typo causing nil error in view. No time to refactor into service or facade

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -5,10 +5,13 @@ class FoodsController < ApplicationController
     conn = Faraday.new(url: "https://api.nal.usda.gov") do |faraday|
       faraday.headers['api_key'] = "X5MqWQCLRSlxUlXnpdm2q51W13hIhOhj5krsN0ep" #ENV['usda_api_key']
     end
+    
     response = conn.get("/fdc/v1/foods/search?query=#{@query}&numberOfResultsPerPage=10&api_key=#{key}")
 
     json = JSON.parse(response.body, symbolize_names: true)
+    
     @num_results = json[:totalHits]
+    
     @foods = json[:foods].map do |food_data|
       Food.new(food_data)
     end
