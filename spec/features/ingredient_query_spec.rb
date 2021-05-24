bf1c2a472ecc645750a89aa69409e7cc367ec7ff
Diana Buffone <diana.pamelaet@gmail.com>
2021-05-24 11:12:41 -0400

    Begin feature test

---
+++ spec/features/ingredient_query_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'Query FoodData Central with one ingredient' do
+  before :each do
+    visit '/'
+    fill_in :q, with: 'sweet potatoes'
+    click_on 'Search'
+  end
+  describe 'Get 10 most relevant foods that include the ingredient' do
+    it 'Redirects to the foods path to show search results' do
+
+      expect(current_path).to eq('/foods')
+      # (sweet potatoes should find more than 30,000 results)
+      expect(page).to have_content('Total search results:')
+    end
+
+    it 'Foods path displays TEN foods containing queried ingredient' do
+
+      expect(page).to have_content('TEN food that contain sweet potatoes')
+    end
+
+    it 'For each food item: GTIN/UPC code, description, Brand Owner, ingredients' do
+    end
+  end
+end
