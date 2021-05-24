00be246c48c6dbf25d29106495da3b4cbc3942be
Klaudia Stewart <klaudiastewart@gmail.com>
2021-05-24 09:11:06 -0600

    Fill in q instead of search

--- spec/features/search_for_sweet_potatoes.rb
+++ spec/features/search_for_sweet_potatoes.rb
@@ -4,9 +4,9 @@ RSpec.describe 'searching for food', type: :feature do
   it 'can search for sweet potatoes' do
     visit root_path
 
-    fill_in :search, with: 'sweet potatoes'
+    fill_in :q, with: 'sweet potatoes'
     click_button 'Search'
 
-    expect(current_page).to eq(foods_path)
+    expect(current_path).to eq(foods_path)
   end
 end
