97ca87c38de3cf882e955e651ee68ed05020ac4f
joemecha <jwmecha@gmail.com>
2021-05-24 11:02:11 -0500

    Build API call in foods controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,7 +1,13 @@
 class FoodsController < ApplicationController 
   def index
-    require 'pry'; binding.pry
-    query = params[:q]  
-  end
+    @query = params[:q]  
+    conn = Faraday.new(url: "https://api.nal.usda.gov") do |faraday|
+      faraday.headers['api_key'] = "X5MqWQCLRSlxUlXnpdm2q51W13hIhOhj5krsN0ep" #ENV['usda_api_key']
+    end
+    response = conn.get("/fdc/v1/foods/search?query=#{@query}&numberOfResultsPerPage=10&#{key}")
 
+    json = JSON.parse(response.body, symbolize_names: true)
+    @foods_info = json[:results].map do |food_data|
+      Food.new(food_data)
+  end
 end
