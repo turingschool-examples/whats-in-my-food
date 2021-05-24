75e65a3ea6cd212c969ca078d0f6c92d340aebb8
Klaudia Stewart <klaudiastewart@gmail.com>
2021-05-24 10:07:49 -0600

    Refactor stub request

--- spec/facades/food_facade_spec.rb
+++ spec/facades/food_facade_spec.rb
@@ -2,16 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'FoodFacade' do
   it "returns an array of food objects" do
-    json_response = File.read('spec/fixtures/food_items.json')
-    stub_request(:get, "https://api.nal.usda.gov/fdc/v1/foods/search?api_key=TETafShutNWdF4DNaCDCPt5vLzinA0S4elKEbloh&query=sweet%20potatoes").
-             with(
-               headers: {
-           	  'Accept'=>'*/*',
-           	  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-           	  'User-Agent'=>'Faraday v1.4.2'
-               }).
-             to_return(status: 200, body: json_response, headers: {})
-
+    stub_food_request
     facade = FoodFacade.get_food_items("sweet potatoes")
     expect(facade).to be_an Array
     expect(facade.first).to be_a FoodItem
