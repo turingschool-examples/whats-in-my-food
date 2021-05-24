478f7a7841f7cdc0f186e6e58527e9c9436bd0fa
joemecha <jwmecha@gmail.com>
2021-05-24 11:29:09 -0500

    Add attributes to Poro, update foods index page

--- app/poros/food.rb
+++ app/poros/food.rb
@@ -1,5 +1,13 @@
 class Food 
+  attr_reader :upc_code,
+              :description, 
+              :brand_owner,
+              :ingredients
+
   def initialize(attributes)
-    
+    @upc_code = attributes[:gtinUpc]
+    @description = attributes[:description]
+    @brand_owner = attributes[:brandOwner]
+    @ingredients = attributes[:ingredients]
   end
 end
--- spec/features/foods/index_spec.rb
+++ spec/features/foods/index_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe 'Foods Index (search results)' do
       fill_in :foods, with: 'sweet potatoes'
       click_button 'Search'
 
+      expect(current_path).to eq(foods_path)
       expect(page.status_code).to eq 200
       expect(page).to have_content("Total items returned: 44128")
       expect(page).to have_content("GTIN/UPC code:")
