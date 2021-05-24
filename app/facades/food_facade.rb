3e3173b1a812c4ab50eb5751ec1dc458c902b028
Joey Haas <75232735+joeyh92989@users.noreply.github.com>
2021-05-24 11:15:53 -0600

    got all tests to pass with new poro, facade,service structure

--- app/facades/food_facade.rb
+++ app/facades/food_facade.rb
@@ -1,7 +1,11 @@
 class FoodFacade
   def self.search(search_params)
     json = FoodService.search_foods(search_params)
-    binding.pry
-    json[:foods].map { |food_info| Food.new(food_info) }
+    foods =json[:foods].map { |food_info| Food.new(food_info) }
+    foods[0..9]
+  end
+  def self.total_results(search_params)
+    json = FoodService.search_foods(search_params)
+    json[:totalHits]
   end
 end
--- spec/Facades/food_facade_spec.rb
+++ spec/Facades/food_facade_spec.rb
@@ -1,10 +1,14 @@
 require "rails_helper"
 
 describe "FoodFacade" do
-  it "returns an array food" do
+  it "returns an array food with only 10 items" do
     facade = FoodFacade.search("Sweet Potatoes")
     expect(facade).to be_an Array
     expect(facade.first).to be_a Food
+    expect(facade.count).to eq(10)
   end
-
+  it "returns total results" do
+  facade = FoodFacade.total_results("Sweet Potatoes")
+  expect(facade).to eq(44128)
+  end
 end
