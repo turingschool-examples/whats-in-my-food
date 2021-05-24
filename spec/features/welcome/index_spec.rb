7b599ee0578609733ee3b64944d97793e99386c4
joemecha <jwmecha@gmail.com>
2021-05-24 10:15:16 -0500

    Add welcome index spec

---
+++ spec/features/welcome/index_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'Welcome page' do
+  describe 'when a user visits the root path' do
+    it 'displays an ingredient search form' do
+      visit root_path
+
+      expect(page).to have_content("Ingredient Search")
+      expect(page).to have_button('Search')
+    end
+
+    it 'user can search for a food by name' do
+      visit root_path
+      fill_in :search, with: 'sweet potatoes'
+      click_button 'Search'
+      expect(current_path).to eq('/foods')
+    end
+  end
+end
