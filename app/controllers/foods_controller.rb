46d0182dd0d9d2bec1d3e6edc967eb7ce66ec081
koshea9 <keegan.oshea9@gmail.com>
2021-05-24 09:24:01 -0600

    add api response for search

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,4 +1,13 @@
 class FoodsController < ApplicationController
   def index
+    search_query = params[:q]
+    conn = Faraday.new(
+      url: "https://api.nal.usda.gov/fdc/",
+      params: {},
+      headers: {"X-Api-Key" => "#{ENV["FOOD_DATA_API_KEY"]}"
+    }
+    )
+    response = conn.get("v1/foods/search/?query=#{search_query}")
+    @foods = JSON.parse(response.body, symbolize_names: true)
   end
 end
