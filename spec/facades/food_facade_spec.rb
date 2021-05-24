f95f9de448ee31ebe3159938a4f245b836ba5235
Diana Buffone <diana.pamelaet@gmail.com>
2021-05-24 12:52:57 -0400

    Add food facade test

--- spec/facades/food_facade_spec.rb
+++ spec/facades/food_facade_spec.rb
@@ -1,9 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'Food Facade' do
-  it 'returns an array of food objects'
-  # create test
+  it 'returns an array of food objects' do
+    ingredient_search = FoodFacade.foods_with_ingredient('Avocado')
 
-  it 'returns an object with total number of results' do 
-  # create test
+    expect(ingredient_search).to be_an(Array)
+    expect(ingredient_search.first).to be_a(Food)
+  end
+
+
+  it 'returns total number of results' do
+    total_results = FoodFacade.total_results_ingredient('Avocado')
+
+    expect(total_results).to be_an(Integer)
+  end
 end
