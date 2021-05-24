38d3f0bccec11ac4e71cf60dd151baa130833a84
George Ho <george.h.w.ho@gmail.com>
2021-05-24 11:10:41 -0400

    Add VCR configuration

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -62,3 +62,13 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+  config.filter_sensitive_data('<DONT_SHARE_MY_FOOD_KEY>') { ENV['food_key'] }
+  config.configure_rspec_metadata!
+  config.default_cassette_options = {
+    re_record_interval: 7.days
+  }
+end
