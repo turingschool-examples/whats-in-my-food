38d78b8b0e0057d9c4f7d3399a962ad79a118a49
Aidan McGuire <>
2021-05-24 10:12:54 -0600

    Refactor api call to search for ingredients

--- spec/features/foods/index_spec.rb
+++ spec/features/foods/index_spec.rb
@@ -4,22 +4,35 @@ RSpec.describe 'foods index' do
   it 'has the total number of items return from search' do
     visit root_path
 
-    fill_in :q, with: 'sweet potatoes'
+    fill_in :q, with: 'sugar'
     click_button 'Search'
 
     expect(current_path).to eq(foods_path)
-    expect(page).to have_content(44128)
+    expect(page).to have_content(187431)
   end
 
   it 'has a list of ten foods that contain the ingredient from search' do
     visit root_path
 
-    fill_in :q, with: 'sweet potatoes'
+    fill_in :q, with: 'sugar'
     click_button 'Search'
 
     expect(current_path).to eq(foods_path)
-    expect(page).to have_content('ARCHER FAMRS')
-    expect(page).to have_content('Spring Acres Sales Company Inc.')
+    expect(page).to have_content('KEY LIME CHEESECAKE MIX')
+    expect(page).to have_content('CREME BRULEE')
   end
-  it 'has attrs for each food'
+  it 'has attrs for results' do
+    visit root_path
+
+    fill_in :q, with: 'sugar'
+    click_button 'Search'
+
+    expect(current_path).to eq(foods_path)
+    within("#food-637921") do
+      expect(page).to have_content('KEY LIME CHEESECAKE MIX')
+      expect(page).to have_content('816277011706')
+      expect(page).to have_content('Pheonix Food LLC')
+      expect(page).to have_content('POWDERED SUGAR (SUGAR, CORNSTARCH), BROWN SUGAR (SUGAR, CANE SYRUP), AND LIME FLAVORING.')
+    end
+  end
 end
