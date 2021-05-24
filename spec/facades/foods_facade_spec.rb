6476e084030a291942806c47d55f344719379ef3
foymikek <foymikek@gmail.com>
2021-05-24 10:09:41 -0600

    Add passing FoodsFacade test, 'can return total food search count'.

--- spec/facades/foods_facade_spec.rb
+++ spec/facades/foods_facade_spec.rb
@@ -14,4 +14,12 @@ describe FoodsFacade do
     expect(foods_facade.count).to eq(50)
     expect(foods_facade.first).to be_an_instance_of(Food)
   end
+
+  it 'can return total food search count' do
+    food_input   =  "sweet potatoes"
+    foods_facade = FoodsFacade.searched_food_count(food_input)
+
+    expect(foods_facade.class).to eq(Integer)
+    expect(foods_facade).to eq(44128)
+  end
 end
