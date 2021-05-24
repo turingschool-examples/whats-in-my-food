2259d88493f4c4465f48544c241921b57b07a572
Klaudia Stewart <klaudiastewart@gmail.com>
2021-05-24 09:57:56 -0600

    Iterate over items

--- app/facades/food_facade.rb
+++ app/facades/food_facade.rb
@@ -1,5 +1,10 @@
 class FoodFacade
+  require "./app/services/food_service.rb"
+
   def self.get_food_items(food)
     items = FoodService.call_for_items(food)
+    items[:foods][0..9].map do |data|
+      FoodItem.new(data)
+    end
   end
 end
