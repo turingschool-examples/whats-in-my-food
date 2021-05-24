e258dd3c98094bfd668e8950239f15bd4f4afc0c
loganjacob76 <nagolisback@gmail.com>
2021-05-24 10:55:22 -0600

    Update facade to use poros

--- app/facades/food_facade.rb
+++ app/facades/food_facade.rb
@@ -2,8 +2,7 @@ class FoodFacade
   class << self
     def foods_by_ingredient(ingredient)
       results = FoodService.search_by_ingredient(ingredient)
-      results[:foods] = results[:foods][0..9]
-      results
+      Search.new(results)
     end
   end
 end
