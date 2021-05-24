1d981898239a7f5f0e8321b964224c52af04f419
Harrison Blake <harrison.r.blake.gmail.com>
2021-05-24 10:16:32 -0600

    Finish mvp of user story

--- spec/features/foods/index_spec.rb
+++ spec/features/foods/index_spec.rb
@@ -18,4 +18,25 @@ describe "As an authenticated user, when I visit the root path and fill in the s
 
     expect(page).to have_content("Total number of items returned: 33074")
   end
+
+  it "display only the top 10 items with sweet potatoes as an ingredient" do
+    visit root_path
+
+    fill_in "q", with: "sweet potatoes"
+    click_button "Search"
+
+    expect(page.all('li', count: 40))
+    expect(page).to have_content("INGREDIENTS: ")
+  end
+
+  it "displays GTIN/UPC, Brand Owner, and Description of food item" do
+    visit root_path
+
+    fill_in "q", with: "sweet potatoes"
+    click_button "Search"
+
+    expect(page).to have_content("Brand Owner: ARCHER FARMS")
+    expect(page).to have_content("GTIN/UPC: 492111402857")
+    expect(page).to have_content("Description: SWEET POTATOES")
+  end
 end
---
+++ spec/poros/food_spec.rb
@@ -0,0 +1,6 @@
+RSpec.describe Food do
+  describe 'happy path' do
+    it 'should build a Film PORO based on given data' do
+    end
+  end
+end
