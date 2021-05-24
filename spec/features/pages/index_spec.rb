b9151141bc7c3c66cfbcef6dea808f92ba0aed49
Andrew Shafer <73140608+Aphilosopher30@users.noreply.github.com>
2021-05-24 09:42:10 -0600

    test it displays the total count all foods with a specific ingrediant

--- spec/features/pages/index_spec.rb
+++ spec/features/pages/index_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'foods index' do
   scenario "user submits valid input" do
     json_response = File.read('spec/fixtures/foods/sweet_potato_search.json')
 
-    stub_request(:get, "https://api.nal.usda.gov/fdc/v1/foods/search?query=sweet potato&api_key=#{ENV['food_api']}").
+    stub_request(:get, "https://api.nal.usda.gov/fdc/v1/foods/search?query=sweet+potato&api_key=#{ENV['food_api']}").
       with(
         headers: {
         }).
@@ -16,6 +16,10 @@ RSpec.describe 'foods index' do
     fill_in 'q', with: "sweet potato"
     click_button "Search"
     expect(current_path).to eq("/foods")
+
+    search = "sweet potato"
+
+    expect(page).to have_content("total foods with #{search}: #{44128}")
 
   end
 
