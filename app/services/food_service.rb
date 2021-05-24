8386d2bae7a3eac504eb62a5d5dadc9a6c05ca17
Klaudia Stewart <klaudiastewart@gmail.com>
2021-05-24 09:58:10 -0600

    Inherit from self

--- app/services/food_service.rb
+++ app/services/food_service.rb
@@ -1,19 +1,21 @@
 class FoodService
-  def call_for_items(food)
-    response = conn.get("/fdc/v1/foods/search?query=#{food}")
-    parse_data(response)
-  end
+  class << self
+    def call_for_items(food)
+      response = conn.get("/fdc/v1/foods/search?query=#{food}")
+      parse_data(response)
+    end
 
-  private
+    private
 
-  def conn
-    Faraday.new(
-      url: "https://api.nal.usda.gov",
-      params: { api_key: ENV['FDA_TOKEN']}
-    )
-  end
+    def conn
+      Faraday.new(
+        url: "https://api.nal.usda.gov",
+        params: { api_key: ENV['FDA_TOKEN']}
+      )
+    end
 
-  def parse_data(response)
-    JSON.parse(response.body, symbolize_names: true)
-  end
+    def parse_data(response)
+      JSON.parse(response.body, symbolize_names: true)
+    end
+  end 
 end
