1025c767d7c257911ec741eaaefa8a125b768df4
Erika Kischuk <erika.kischuk@gmail.com>
2021-10-25 14:03:03 -0600

    Add test for food index page

---
+++ spec/features/foods/index_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe 'food index page' do
+  
+end
--- spec/poros/food_item_spec.rb
+++ spec/poros/food_item_spec.rb
@@ -2,11 +2,16 @@ require 'rails_helper'
 
 RSpec.describe FoodItem do
   it 'exists and has attributes' do
-      food_info = {gtinUpc: "86578968",
+    food_info = {gtinUpc: "86578968",
       description: "its food",
       brandOwner: "probably nestle",
       ingredients: 'potato'
       }
     food_item = FoodItem.new(food_info)
+
+    expect(food_item.gtin_upc).to eq(food_info[:gtinUpc])
+    expect(food_item.description).to eq(food_info[:description])
+    expect(food_item.brand_owner).to eq(food_info[:brandOwner])
+    expect(food_item.ingredients).to eq(food_info[:ingredients])
   end
 end
