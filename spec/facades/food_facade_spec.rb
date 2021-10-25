43661f6eb29b9820d51fc730baf88a9d24542391
Weston Ellis <ellisweston112@gmail.com>
2021-10-25 10:48:31 -0600

    Update test

--- spec/facades/food_facade_spec.rb
+++ spec/facades/food_facade_spec.rb
@@ -1,11 +1,12 @@
 require "rails_helper"
 
 RSpec.describe FoodFacade do
-  it 'can make food objects' do
+  it 'can return results' do
     results = FoodFacade.food_search('sweet potatoes')
 
-    expect(results).to be_a(Array)
-    expect(results.length).to eq(10)
-    expect(results.first).to be_a(Food)
+    expect(results).to be_a(SearchResult)
+    expect(results.total_results).to be_a(Integer)
+    expect(results.foods.length).to eq(10)
+    expect(results.foods.first).to be_a(Food)
   end
 end
