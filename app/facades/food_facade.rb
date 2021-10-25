1543e69a5a74368bdc49b8ff73bee5955ed1887c
sami-p <sami.peterson14@gmail.com>
2021-10-25 10:55:50 -0600

    Add total hits facade method

--- app/facades/food_facade.rb
+++ app/facades/food_facade.rb
@@ -1,9 +1,13 @@
 class FoodFacade
 
   def self.get_10(ingredient)
-    json = FoodDataService.search_ingredient(ingredient)
+    json = FoodDataService.search_ingredient(ingredient)[:foods][0..9]
     json.map do |food|
       Food.new(food)
     end
+  end
+
+  def self.total_hits(ingredient)
+    json = FoodDataService.search_ingredient(ingredient)[:totalHits]
   end
 end
