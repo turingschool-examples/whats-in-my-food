0a1955a748e275990f82361b7176c0d4e84cf764
Henry Schmid <henry.schmid1@gmail.com>
2021-10-25 09:35:08 -0600

    Add test for number of items returned

--- spec/features/foods/index_spec.rb
+++ spec/features/foods/index_spec.rb
@@ -6,4 +6,10 @@ describe 'foods index' do
     fill_in :q, with: 'sweet potatoes'
     click_button 'Search'
   end
+
+
+  it 'shows total number of items returned' do
+    expect(current_path).to eq(foods_path)
+    expect(page).to have_content("Total Results: 46,320")
+  end
 end
