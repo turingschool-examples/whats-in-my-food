5c9bce6e7d5c9bd8b23dc4df9acdadf86105c3cf
sami-p <sami.peterson14@gmail.com>
2021-10-25 09:21:29 -0600

    Test search function on welcome page

---
+++ spec/features/welcome/index_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe 'Index Page' do
+  describe 'search form' do
+    it 'has search form to find first 10 relevant foods by ingredient' do
+      visit root_path
+      save_and_open_page
+
+      expect(current_path).to eq(root_path)
+    end
+  end
+end
