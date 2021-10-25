4149df0f45b56061ffa855a6270044f3ac1f59d4
Tanner Dale <tanner@tannerdale.com>
2021-10-25 11:35:28 -0500

    Update test to be more specific

--- spec/facades/food_facade_spec.rb
+++ spec/facades/food_facade_spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
 
 describe FoodFacade, :vcr do
+  let(:result) { FoodFacade.search('sweet potatoes') }
+
   it 'returns an array of poros' do
-    result = FoodFacade.search('sweet potatoes')
     all_poros = result.first.all? { |r| r.is_a?(FoodPoro) }
 
-    expect(result).to be_an(Array)
-
     expect(all_poros).to be(true)
+  end
+
+  it 'has the total hits' do
     expect(result.last).to eq(46_320)
   end
 end
