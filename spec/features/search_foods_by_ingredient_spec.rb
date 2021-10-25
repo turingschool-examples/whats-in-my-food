7acb1302b36714544e0176ee37363b32a8fed1cc
Michael Mattson <michael.p.mattson@gmail.com>
2021-10-25 10:43:03 -0600

    Change to table, update tests, test number of rows including header row

--- spec/features/search_foods_by_ingredient_spec.rb
+++ spec/features/search_foods_by_ingredient_spec.rb
@@ -12,27 +12,21 @@ RSpec.describe 'welcome page' do
   end
 
   it 'has a total of the number of items returned' do
-    # Then I should see a total of the number of items returned by the search.
-    # (sweet potatoes should find more than 30,000 results)
     expect(page).to have_content('Total Foods: 46320')
   end
 
-  xit 'has a list of ten foods that contain the ingredient' do
-    # Then I should see a list of TEN foods that contain the ingredient "sweet potatoes"
-    # expect(page).to have_section('food-1')
+  it 'has a list of ten foods that contain the ingredient' do
+    within '#foods' do
+      expect(all('tr').count).to eq(11)
+    end
   end
 
   it 'has attributes for each listed food' do
-    # And for each of the foods I should see:
-    # - The food's GTIN/UPC code
-    # - The food's description
-    # - The food's Brand Owner
-    # - The food's ingredients
     within '#food-1929370' do
-      expect(page).to have_content("GTIN/UPC: 076700002019")
-      expect(page).to have_content("Description: SWEET POTATOES")
-      expect(page).to have_content("Brand Owner: John W. Taylor Packing Co. Inc")
-      expect(page).to have_content("Ingredients: SWEET POTATOES.")
+      expect(page).to have_content("076700002019")
+      expect(page).to have_content("SWEET POTATOES")
+      expect(page).to have_content("John W. Taylor Packing Co. Inc")
+      expect(page).to have_content("SWEET POTATOES.")
     end
   end
 end
