c94ca62863f2b259d2d369e52c898510898e25db
Ida Olson <idaolson@gmail.com>
2021-10-25 09:50:09 -0600

    Create welcome page features test

---
+++ spec/features/welcome/index_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe "welcome and food search" do
+  it "text" do
+    visit root_path
+
+
+
+    expect(current_path).to eq(foods_path)
+  end
+
+end
+
+
+
+# As a user,
+# When I visit "/"
+# And I fill in the search form with "sweet potatoes"
+# (Note: Use the existing search form)
+# And I click "Search"
+# Then I should be on page "/foods"
+# Then I should see a total of the number of items returned by the search.
+# (sweet potatoes should find more than 30,000 results)
+# Then I should see a list of TEN foods that contain the ingredient "sweet potatoes"
+# And for each of the foods I should see:
+# - The food's GTIN/UPC code
+# - The food's description
+# - The food's Brand Owner
+# - The food's ingredients
