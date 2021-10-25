10cd6fcd082dad3448a92e0881fd8fe87127aca6
Weston Ellis <ellisweston112@gmail.com>
2021-10-25 10:17:58 -0600

    Write facade test

---
+++ spec/facades/food_facade_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+RSpec.describe FoodFacade do
+  it 'can make food objects' do
+    results = FoodFacade.food_search(query)
+
+    expect(results).to be_a(Array)
+    expect(results.length).to eq(10)
+    expect(results.first).to be_a(Food)
+  end
+end
