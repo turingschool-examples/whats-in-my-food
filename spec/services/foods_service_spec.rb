ffb708764416a7426897eaa878ee696235e3e0ea
Michael Mattson <michael.p.mattson@gmail.com>
2021-10-25 10:50:07 -0600

    Add service test for foods_query

--- spec/services/foods_service_spec.rb
+++ spec/services/foods_service_spec.rb
@@ -1,23 +1,18 @@
-# require 'rails_helper'
-#
-# describe FoodsService do
-#   it 'can get all food data' do
-#     response = FoodsService.foods_query('sweet potatoes')
-#
-#
-#
-#     # expect(response).to be_a(Hash)
-#     # expect(response[:results][0][:members]).to be_an(Array)
-#     #
-#     # senate_member_data = response[:results][0][:members].first
-#     #
-#     # expect(senate_member_data).to have_key(:first_name)
-#     # expect(senate_member_data[:first_name]).to be_a(String)
-#     #
-#     # expect(senate_member_data).to have_key(:last_name)
-#     # expect(senate_member_data[:last_name]).to be_a(String)
-#     #
-#     # expect(senate_member_data).to have_key(:twitter_account)
-#     # expect(senate_member_data[:twitter_account]).to be_a(String)
-#   end
-# end
+require 'rails_helper'
+
+describe FoodsService do
+  it 'can get all food data' do
+    response = FoodsService.foods_query('sweet potatoes')
+
+    expect(response).to be_a(Hash)
+    expect(response[:totalHits]).to be_an(Integer)
+    foods_data = response[:foods]
+
+    expect(foods_data).to be_an(Array)
+    expect(foods_data.first).to have_key(:fdcId)
+    expect(foods_data.first).to have_key(:gtinUpc)
+    expect(foods_data.first).to have_key(:description)
+    expect(foods_data.first).to have_key(:brandOwner)
+    expect(foods_data.first).to have_key(:ingredients)
+  end
+end
