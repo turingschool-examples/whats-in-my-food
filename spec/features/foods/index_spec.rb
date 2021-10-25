e07d0b12bba8ca22b5addd755a089d55f2ad850b
Henry Schmid <henry.schmid1@gmail.com>
2021-10-25 10:38:45 -0600

    Add test for 10 results in view

--- spec/features/foods/index_spec.rb
+++ spec/features/foods/index_spec.rb
@@ -5,11 +5,23 @@ describe 'foods index' do
     visit root_path
     fill_in :q, with: 'sweet potatoes'
     click_button 'Search'
+    @foods = FoodFacade.find_foods('sweet potatoes')
   end
 
 
   it 'shows total number of items returned' do
     expect(current_path).to eq(foods_path)
-    expect(page).to have_content("Total Results: 46,320")
+    expect(page).to have_content("Total Results: #{@foods.first.total_hits}")
+  end
+
+  it 'shows 10 results' do
+    @foods[0..9].each do |food|
+      expect(page).to have_content(food.gtin_upc)
+      expect(page).to have_content(food.description)
+      expect(page).to have_content(food.brand_owner)
+      expect(page).to have_content(food.ingredients)
+    end
+
+    expect(page).to_not have_content(@foods[10].gtin_upc)
   end
 end
