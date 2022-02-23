7c220c09c9ba76144e8c1a8c15eb9c26cfb6dd40
Kevin Gloss <kgloss89@gmail.com>
2022-02-23 09:34:40 -0700

    Test service spec

---
+++ spec/services/food_service_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'Food Service' do 
+  describe 'searches for food', :vcr do 
+    food_results = FoodService.search_foods('sweet potatoes')
+    foods = food_results[:foods]
+
+    expect(food_results[:totalHits]).to eq(48008)
+    expect(foods).to be_a(Array)
+    # expect(foods.count).to eq(10)
+    expect(foods.first).to have_key(:fdcId)
+    expect(foods.first).to have_key(:description)
+    expect(foods.first).to have_key(:brandOwner)
+    expect(foods.first).to have_key(:ingredients)
+  end
+end
