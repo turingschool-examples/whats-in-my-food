0f16246b95dfbded559125e0d662659a836973fa
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-02-23 11:02:39 -0600

    Service test complete

--- spec/services/food_service_spec.rb
+++ spec/services/food_service_spec.rb
@@ -3,12 +3,47 @@ require 'rails_helper'
 RSpec.describe FoodService do
   describe 'class methods' do
     describe 'foods search' do
+
+      before(:each) do
+        VCR.insert_cassette('sweet_potato_search')
+      end
+      after(:each) do
+        VCR.eject_cassette('sweet_potato_search')
+      end
+      
       let!(:search) {FoodService.foods_search('sweet potato')}
       let!(:result) {search[:foods].first}
 
       it 'returns response hash' do
         expect(search).to be_a(Hash)
       end
+
+      it 'has total results count' do
+        expect(search).to have_key(:totalHits)
+      end
+
+      it 'contains results array' do
+        expect(search).to have_key(:foods)
+        expect(search[:foods]).to be_an(Array)
+      end
+
+      it 'results array elements contain food data hash' do
+        expect(result).to be_a(Hash)
+      end
+
+      it 'element hash contains correct data' do
+        expect(result).to have_key(:gtinUpc)
+        expect(result[:gtinUpc]).to be_a(String)
+
+        expect(result).to have_key(:description)
+        expect(result[:description]).to be_a(String)
+
+        expect(result).to have_key(:brandOwner)
+        expect(result[:brandOwner]).to be_a(String)
+
+        expect(result).to have_key(:ingredients)
+        expect(result[:ingredients]).to be_a(String)
+      end
     end
   end
-end 
+end
