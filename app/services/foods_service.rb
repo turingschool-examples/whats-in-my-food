4a63281e8abaa7609de97c805729776f41f9b38e
Kelly Anderson <kean9528@colorado.edu>
2022-02-23 09:57:31 -0700

    write tests for service

---
+++ app/services/foods_service.rb
@@ -0,0 +1,11 @@
+class FoodsService
+  def self.conn
+    url = 'https://api.nal.usda.gov/fdc/v1/foods/'
+    Faraday.new(url: url, params: { api_key: ENV['foods_api_key'] })
+  end
+
+  def self.get_foods(string)
+    response = conn.get('search', { query: string})
+    JSON.parse(response.body, symbolize_names: true)[:results])
+  end
+end
---
+++ spec/services/foods_service_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe FoodsService, type: :service do
+  context 'class methods', :vcr do
+    describe '::conn' do
+      it 'returns Faraday connection' do
+        foods = FoodsService.conn
+
+        expect(foods).to be_a(Faraday::Connection)
+      end
+    end
+
+    describe '::get_foods' do
+     it 'returns hash of foods details' do
+       foods = FoodsService.get_foods(11)
+
+       expect(foods).to be_a(Hash)
+     end
+   end
+  end
+end
