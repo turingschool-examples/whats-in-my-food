8409c4570b4ffda169ed39e2bc0f729ba746fcc9
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-02-23 12:48:18 -0500

    Refactor index method

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,17 +1,5 @@
 class FoodsController < ApplicationController
   def index
-    result = params[:q]
-    conn = Faraday.new(url: 'https://api.nal.usda.gov/fdc/v1/') do |faraday|
-        faraday.headers['X-Api-Key'] = ENV['api_key']
-    end
-    response = conn.get('foods/search') do |request|
-        request.headers['X-Api-Key'] = ENV['api_key']
-        request.params['query'] = result
-        request.params['pageSize'] = 10
-    end
-    json = JSON.parse(response.body, symbolize_names: true)
-    @food_results = json[:foods].map do |food_data|
-        Food.new(food_data)
-    end
+    @food_results = FoodsFacade.search_results(params[:q])
   end
 end
