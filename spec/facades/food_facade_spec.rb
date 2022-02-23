a566a7462f903e78d1b80cc19b51d4d879fe10fc
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-02-23 10:02:19 -0700

    Add VCR to test

--- spec/facades/food_facade_spec.rb
+++ spec/facades/food_facade_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe FoodFacade do
   context 'instance methods' do
     context '#find_food(food)' do
-      it 'returns Food data' do
+      it 'returns Food data', :vcr do
         query = FoodFacade.new
         results = query.find_food("sweet potatoes")
 
-        expecrt(results).to be_a(Array)
+        expect(results).to be_a(Array)
         results.each do |result|
           expect(result).to be_a(Food)
-        end 
+        end
       end
     end
   end
