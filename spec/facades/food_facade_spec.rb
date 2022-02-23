41feb595692ed0ae88eea68f623eac47ff711697
Kevin Gloss <kgloss89@gmail.com>
2022-02-23 10:38:19 -0700

    Update test to check for total results

--- spec/facades/food_facade_spec.rb
+++ spec/facades/food_facade_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe FoodFacade do 
-  it 'finds the first to food items for a search' do 
+  it 'finds the first ten food items for a search' do 
     foods = FoodFacade.search_foods('sweet potatoes')
 
-    expect(foods.first).to be_an_instance_of(Food)
-    expect(foods.first.description).to eq("SWEET POTATOES")
-    expect(foods.count).to eq(10)
+    expect(foods[:foods].first).to be_an_instance_of(Food)
+    expect(foods[:foods].first.description).to eq("SWEET POTATOES")
+    expect(foods[:foods].count).to eq(10)
+    expect(foods[:total_results]).to eq(48008)
   end
 end
