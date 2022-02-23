36f118adbe11a69a87343ade78fc7f3239b23753
Kelly Anderson <kean9528@colorado.edu>
2022-02-23 09:31:31 -0700

    add functionality to reach foods index

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 RSpec.describe 'search index' do
   it 'has a search field for ingredients' do
     visit root_path
-    fill_in :search, with: 'Sweet Potatoes'
+    fill_in :q, with: 'Sweet Potatoes'
     click_button 'Search'
     expect(current_path).to eq('/foods')
   end
