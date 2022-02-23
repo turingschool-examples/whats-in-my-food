32ce4dddbde131156bfea638a86b45db1ad5dec5
Kelly Anderson <kean9528@colorado.edu>
2022-02-23 09:39:47 -0700

    make service for foods

---
+++ spec/features/foods/index_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+RSpec.describe 'search index' do
+  it 'returns search results' do
+    visit root_path
+    fill_in :q, with: 'Sweet Potatoes'
+    click_button 'Search'
+    expect(current_path).to eq('/foods')
+    expect(page).to have_content("Total")
+  end
+end
