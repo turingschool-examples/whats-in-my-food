ece30519646f2b16cecae390adaa70cf47a1bd6a
Kevin Gloss <kgloss89@gmail.com>
2022-02-23 10:38:42 -0700

    Update food poro to include gtinUpc

--- spec/poros/food_spec.rb
+++ spec/poros/food_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Food do 
   it 'has attributes' do 
     data = {
-      fdcId: 983525,
+      gtinUpc: "8901020020844",
       description: "SWEET POTATOES",
       brandOwner: "NOT A BRANDED ITEM",
       ingredients: "ORGANIC SWEET POTATOES."
@@ -12,6 +12,7 @@ RSpec.describe Food do
     food = Food.new(data)
 
     expect(food).to be_an_instance_of(Food)
+    expect(food.gtinUpc).to eq("8901020020844")
     expect(food.description).to eq("SWEET POTATOES")
     expect(food.brand_owner).to eq("NOT A BRANDED ITEM")
     expect(food.ingredients).to eq("ORGANIC SWEET POTATOES.")
