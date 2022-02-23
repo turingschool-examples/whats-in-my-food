2c778127b0918bae80f95fac66427cd1488ff65c
Seth Perna <gseth26@gmail.com>
2022-02-23 12:41:45 -0500

    finish all poro, facade and service testing. Work on counting results on page

--- spec/poros/food_spec.rb
+++ spec/poros/food_spec.rb
@@ -5,33 +5,15 @@ RSpec.describe Food do
       data = {
         fdcId: '123456',
         description: 'creamy beige',
-        brand: 'baileys irish cream',
-        operatingHours: [
-          {
-            standardHours: {
-              monday: "All Day",
-              tuesday: "All Day",
-              wednesday: "All Day",
-              thursday: "All Day",
-              friday: "All Day",
-              saturday: "All Day",
-              sunday: "All Day"
-            }
-          }
-        ]
+        brandOwner: 'baileys irish cream',
+        ingredients: 'regret'
       }
-      park = Park.new(data, 33)
-      expect(park.name).to eq(data[:fullName])
-      expect(park.description).to eq(data[:description])
-      expect(park.directions).to eq(data[:directionsInfo])
-      expect(park.park_count).to eq(33)
-      expect(park.monday).to eq(data[:operatingHours][0][:standardHours][:monday])
-      expect(park.tuesday).to eq(data[:operatingHours][0][:standardHours][:tuesday])
-      expect(park.wednesday).to eq(data[:operatingHours][0][:standardHours][:wednesday])
-      expect(park.thursday).to eq(data[:operatingHours][0][:standardHours][:thursday])
-      expect(park.friday).to eq(data[:operatingHours][0][:standardHours][:friday])
-      expect(park.saturday).to eq(data[:operatingHours][0][:standardHours][:saturday])
-      expect(park.sunday).to eq(data[:operatingHours][0][:standardHours][:sunday])
+      food = Food.new(data, 33)
+      expect(food.code).to eq(data[:fdcId])
+      expect(food.description).to eq(data[:description])
+      expect(food.brand).to eq(data[:brandOwner])
+      expect(food.ingredients).to eq(data[:ingredients])
+      expect(food.total_count).to eq(33)
     end
   end
 end
