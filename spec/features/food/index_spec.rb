d74b503c5d778af9c8983c4a556e85b389bb6729
Karan Mehta <karanm645@gmail.com>
2022-02-23 11:43:07 -0600

    Update spec files

---
+++ spec/features/food/index_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "food index page" do 
+  it 'shows the total number of items returned by the search' do 
+    visit "/foods"
+  end 
+
+  it 'shows food attributes' do 
+
+  end 
+end 
