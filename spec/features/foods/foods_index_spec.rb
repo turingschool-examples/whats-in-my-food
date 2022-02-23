4c9b8e2d0657cb8d18af8f4f92e499c28487d6da
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-02-23 10:53:18 -0700

    Refactor test to exclude a random API call

--- spec/features/foods/foods_index_spec.rb
+++ spec/features/foods/foods_index_spec.rb
@@ -1,30 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe 'Foods index page' do
-  before :each do
+
+  it 'displays the total number of items returned by the search', :vcr do
     visit root_path
     fill_in :q, with: "sweet potatoes"
     click_on "Search"
-  end
 
-  it 'displays the total number of items returned by the search', :vcr do
-    visit foods_path
-
-    expect(page).to have_content("Total Hits: 396437")
-    #This should not be. I have no idea where that number is coming from.
+    expect(page).to have_content("Total Hits: 48008")
   end
 
   it 'lists the first 10 items returned in the search', :vcr do
-    visit foods_path
+    visit root_path
+    fill_in :q, with: "sweet potatoes"
+    click_on "Search"
 
     expect(page).to have_content("Results on this page: 10")
   end
 
   it 'lists the foods attributes', :vcr do
-    visit foods_path
+    visit root_path
+    fill_in :q, with: "sweet potatoes"
+    click_on "Search"
 
     #first item
-    within("#foods") do
+    within("#foods_0") do
       expect(page).to have_content("Description: SWEET POTATOES")
       expect(page).to have_content("Brand Owner: NOT A BRANDED ITEM")
       expect(page).to have_content("GTIN/UPC code: 8901020020844")
@@ -32,11 +32,11 @@ RSpec.describe 'Foods index page' do
     end
 
     #last item
-    within("#foods") do
+    within("#foods_9") do
       expect(page).to have_content("Description: SWEET POTATOES")
       expect(page).to have_content("Brand Owner: N&W Farm Produce Inc.")
       expect(page).to have_content("GTIN/UPC code: 882495000013")
-      expect(page).to have_content("Ingredients: ")
+      expect(page).to have_content("Ingredients:")
     end
   end
 end
