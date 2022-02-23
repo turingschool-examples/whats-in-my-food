3e7da26336616b684b94b59fd13774d09c52423a
Robin Dittrich <robinodittrich@gmail.com>
2022-02-23 09:20:48 -0800

    Add shoulda matchers config to rails helper.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'simplecov'
+SimpleCov.start
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
@@ -61,4 +63,19 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include Capybara::DSL
 end
+
+Shoulda::Matchers.configure do |config|
+   config.integrate do |with|
+     with.test_framework :rspec
+     with.library :rails
+   end
+ end
+
+# VCR.configure do |config|
+#   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+#   config.hook_into :webmock
+#   config.filter_sensitive_data('tmdb_key') { ENV['tmdb_key'] }
+#   config.configure_rspec_metadata!
+# end
