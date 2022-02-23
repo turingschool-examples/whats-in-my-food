8e28eb14a17a37c49a7ca36a56f53c2c99a20e26
Sierra Tucker <sierra.tucker121@gmail.com>
2022-02-23 09:28:16 -0700

    Test: Outline of feature test and set up for foods index

---
+++ spec/features/foods/index_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe 'Foods Index Page' do
+  describe 'from the root path, visitor fills in search form' do
+    it 'redirects to the foods index and sees results of the search' do
+      visit root_path
+      fill_in :q, with: 'sweet potatoes'
+      click_on "Search"
+
+      expect(current_path).to eq(foods_path)
+    end
+  end
+end
