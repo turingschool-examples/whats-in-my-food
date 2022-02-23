8e80b4c4552bf0e557a6c940ccd769049b389873
Joel Grant <joelmgrant@me.com>
2022-02-23 10:56:36 -0700

    Modify service and view

--- app/services/food_service.rb
+++ app/services/food_service.rb
@@ -1,7 +1,8 @@
 class FoodService
   def self.search_all_items(search)
     search = ERB::Util.url_encode search
-    response = Faraday.get("https://api.nal.usda.gov/fdc/v1/foods/search?api_key=#{ENV['API_KEY']}&query=#{search}&pageSize=10")
+    response = Faraday.get("https://api.nal.usda.gov/fdc/v1/foods/search?api_key=#{ENV['API_KEY']}&query=#{search}")
+    # response = Faraday.get("https://api.nal.usda.gov/fdc/v1/foods/search?api_key=#{ENV['API_KEY']}&query=#{search}&pageSize=10")
     result = JSON.parse(response.body, symbolize_names: true)
   end
 end
