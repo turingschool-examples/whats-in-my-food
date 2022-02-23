8d17c1b5c423e1395a58da96748cf1ab5c2bc7f0
Jackson Valdez <jacksonvaldez64@gmail.com>
2022-02-23 10:57:30 -0700

    Change ternary operator to if statement

--- app/facades/food_facade.rb
+++ app/facades/food_facade.rb
@@ -2,7 +2,9 @@ class FoodFacade
 
   def self.search_foods(query, page_num)
     FoodService.search_foods(query, page_num)[:foods].map do |food_data|
-      food_data[:ingredients].downcase.include?(query.downcase) ? Food.new(food_data) : nil
+      if food_data[:ingredients].downcase.include?(query.downcase)
+        Food.new(food_data)
+      end
     end.compact
   end
 
