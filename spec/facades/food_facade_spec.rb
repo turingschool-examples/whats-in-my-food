2f1c1d48c15a3fdd3e2f6cbfbba5956e76977cbd
Kevin Gloss <kgloss89@gmail.com>
2022-02-23 10:04:29 -0700

    Test food facade

--- spec/facades/food_facade_spec.rb
+++ spec/facades/food_facade_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe FoodFacade do 
   it 'finds the first to food items for a search' do 
-    
+    foods = FoodFacade.search_foods('sweet potatoes')
+
+    expect(foods.first).to be_an_instance_of(Food)
+    expect(foods.first.description).to eq("SWEET POTATOES")
+    expect(foods.count).to eq(10)
   end
 end
