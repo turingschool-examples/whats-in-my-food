0a1cca688a418b743710ab41cae29a47c8b64a95
Mallory Vining <mallorygallery12@gmail.com>
2022-02-23 09:28:44 -0700

    Setup and features test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -62,3 +62,12 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+  config.filter_sensitive_data('fake_api_key') { ENV['food_api_key'] }
+  config.default_cassette_options = { re_record_interval: 30.days }
+  config.configure_rspec_metadata!
+  config.allow_http_connections_when_no_cassette = true
+end
