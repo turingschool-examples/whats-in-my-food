4d403b442d050b4773b8aa2c211284c045f56728
Joel Grant <joelmgrant@me.com>
2022-02-23 10:56:19 -0700

    Add facade method to provide a count number

--- app/facades/food_facade.rb
+++ app/facades/food_facade.rb
@@ -1,8 +1,12 @@
 class FoodFacade
   def self.food_search(food)
-    search_results = FoodService.search_all_items(food)
-    search_results[:foods].map do |food|
-      FoodItem.new(food)
-    end
+    @total_search_results = FoodService.search_all_items(food)
+    # Using this to limit to ten results before making the poros
+    ten_results = @total_search_results[:foods].map { |result| result }.take(10)
+    ten_results.map { |food| FoodItem.new(food) }
+  end
+
+  def self.count
+    @total_search_results[:totalHits]
   end
 end
