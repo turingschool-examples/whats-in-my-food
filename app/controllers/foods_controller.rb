3bea0389d92b423e3b6c95e9351bd8b8543ac4f9
Jackson Valdez <jacksonvaldez64@gmail.com>
2022-02-23 10:40:30 -0700

    Complete Mid Mod

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,6 +1,6 @@
 class FoodsController < ApplicationController
   def index
-    @foods = FoodFacade.search_foods(params[:search_query], 1).first(10)
-    @num_results = FoodFacade.count_results(params[:search_query], 1)
+    @foods = FoodFacade.search_foods(params[:search_query], 25).first(10)
+    @num_results = FoodFacade.count_results(params[:search_query], 25)
   end
 end
--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -19,25 +19,24 @@ RSpec.describe 'Landing Page' do
   end
 
   it 'it displays a total of the number of items returned by the search' do
-    fill_in :search_query, with: 'something'
+    fill_in :search_query, with: 'sweet potatoes'
     click_on "Search"
 
-    expect(page).to have_content("")
+    expect(page).to have_content("Search Results: 46770")
   end
 
   it 'should display 10 foods that contain the ingredient sweet potatoes' do
     fill_in :search_query, with: 'sweet potatoes'
     click_on "Search"
 
-
     expect(page).to have_content("Search Results: 46770")
 
     expect(page).to have_content("Food's that contain sweet potatoes:")
 
-    expect(page).to have_content("GTIN/UPC: ")
-    expect(page).to have_content("Description: ")
-    expect(page).to have_content("Brand Owner: ")
-    expect(page).to have_content("Ingredients: ")
+    expect(page).to have_content("GTIN/UPC: 028400015233")
+    expect(page).to have_content("Description: LIGHTLY SALTED SWEET & SMOKY BARBECUE FLAVORED KETTLE COOKED POTATO CHIPS, LIGHTLY SALTED SWEET & SMOKY BARBECUE")
+    expect(page).to have_content("Brand Owner: Frito-Lay Company")
+    expect(page).to have_content("Ingredients: POTATOES, VEGETABLE OIL (SUNFLOWER, CORN, AND/OR CANOLA OIL), SWEET & SMOKEY BBQ SEASONING (SUGAR, MALTODEXTRIN [MADE FROM CORN], BROWN SUGAR")
   end
 
 end
