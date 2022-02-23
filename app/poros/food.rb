07c7b9ba45203a1eb9136d95eb14c038f3a92032
Kevin Gloss <kgloss89@gmail.com>
2022-02-23 10:38:56 -0700

    Correct attribute to gtinUpc

--- app/poros/food.rb
+++ app/poros/food.rb
@@ -1,8 +1,8 @@
 class Food 
-  attr_reader :fdcId, :description, :brand_owner, :ingredients
+  attr_reader :gtinUpc, :description, :brand_owner, :ingredients
 
   def initialize(food)
-    @fdcId = food[:fdcId]
+    @gtinUpc = food[:gtinUpc]
     @description = food[:description]
     @brand_owner = food[:brandOwner]
     @ingredients = food[:ingredients]
