522566d11fa0178df337a5e637d8f970772e56c5
Karan Mehta <karanm645@gmail.com>
2022-02-23 12:00:47 -0600

    update spec

--- spec/features/food/index_spec.rb
+++ spec/features/food/index_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe "food index page" do 
   it 'shows the total number of items returned by the search' do 
     visit "/foods"
+
+    expect(page.status_code).to eq 200
   end 
 
   it 'shows food attributes' do 
