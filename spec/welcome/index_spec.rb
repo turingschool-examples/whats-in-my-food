2242cf47470859745e85b95a1216b8345d0a9467
Brad Breiten <jonathan@breiten.us>
2022-02-23 09:32:54 -0700

    Add test for filling in form

--- spec/welcome/index_spec.rb
+++ spec/welcome/index_spec.rb
@@ -3,15 +3,22 @@ require 'rails_helper'
 RSpec.describe 'Welcome Page', type: :feature do
   context 'As a user' do
     context 'When I visit /' do
-      scenario 'I can see a form and search button' do
-        visit root_path
+      before(:each) { visit root_path }
 
+      scenario 'I can see a form and search button' do
         expect(page).to have_current_path('/')
         expect(page).to have_field(:q)
         expect(page).to have_button('Search')
       end
 
-      scenario 'When I fill in the form and search I am taken to /foods'
+      scenario 'When I fill in the form and search I am taken to /foods' do
+        fill_in :q, with: 'sweet potatoes'
+        click_button('Search')
+
+        expect(page).to have_current_path('/foods')
+      end
+
+
       scenario 'On /foods I should see a total number of items returned by the search'
       scenario 'On /foods I should see a list of ten foods that contain that ingredient'
       scenario 'On /foods I should see attributes for each of the ten foods'
