4281d6a5dd709ee1d9eb4d94c3eeaa4154bddc6c
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-02-23 12:33:16 -0500

    Add test to search for keywords

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -7,5 +7,12 @@ RSpec.describe 'Whats In My Food Welcome page' do
     expect(page).to have_button('Search')
   end
 
+  it 'searches for sweet potatoes' do
+    visit ('/')
 
+    fill_in :q, with: 'sweet potatoes'
+    click_button 'Search'
+    expect(current_path).to_not eq('/')
+    
+  end
 end
