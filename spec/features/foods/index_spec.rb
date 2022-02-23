31e404634cb4acfae4603f9752d6a4a1fed961da
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-02-23 09:29:04 -0700

    Add draft of test for search results by food type

--- spec/features/foods/index_spec.rb
+++ spec/features/foods/index_spec.rb
@@ -1,14 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe 'Foods Index Page' do
+  xit 'shows the total number of items returned by the search' do
+    visit '/foods'
+    #  (sweet potatoes should find more than 30,000 results)
+    expect(page).to have_content("Total items found: 30,000")
+    expect(page).to have_content('.foods', count: 10)
+  end
 
+  xit 'shows a list of TEN foods containing sweet potatoes and their details' do
+    visit '/foods'
+
+    within(first('.foods')) do
+      expect(page).to have_css('.GTIN/UPC code')
+      expect(page).to have_css('.description')
+      expect(page).to have_css('.Brand Owner')
+      expect(page).to have_css('.ingredients')
+    end
+    #  Then I should see a list of TEN foods that contain the ingredient "sweet potatoes"
+    # And for each of the foods I should see:
+    #  - The food's GTIN/UPC code
+    #  - The food's description
+    #  - The food's Brand Owner
+    #  - The food's ingredients
+  end
 end
-
-#  Then I should see a total of the number of items returned by the search.
-#  (sweet potatoes should find more than 30,000 results)
-#  Then I should see a list of TEN foods that contain the ingredient "sweet potatoes"
-# And for each of the foods I should see:
-#  - The food's GTIN/UPC code
-#  - The food's description
-#  - The food's Brand Owner
-#  - The food's ingredients
