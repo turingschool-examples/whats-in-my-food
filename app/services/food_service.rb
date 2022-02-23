24d9231ef37f58f8955333f3979d479ea8b9c60d
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-02-23 11:23:56 -0600

    Service class methods, tests passing

--- app/services/food_service.rb
+++ app/services/food_service.rb
@@ -1,8 +1,11 @@
 class FoodService
 
+  def self.foods_search(query)
+    get_url("/search?query=#{query}&api_key=#{ENV['foods_api_key']}")
+  end 
 
   def self.get_url(url)
-    response = Faraday.get("https://api.nal.usda.gov/fdc/v1/foods/#{url}")
+    response = Faraday.get("https://api.nal.usda.gov/fdc/v1/foods#{url}")
     parsed = JSON.parse(response.body, symbolize_names: true)
   end
 end
