f7bfb76497b7723e1e08852e9e781b27ce425981
Sandisz Thieme <sandiszthieme@gmail.com>
2022-05-27 10:49:58 -0600

    FoodFacade#search returns 10 food poros

--- app/facades/food_facade.rb
+++ app/facades/food_facade.rb
@@ -1,6 +1,6 @@
 class FoodFacade
   def self.search(keyword)
-    FoodService.search(keyword)[:foods].map do |data|
+    FoodService.search(keyword)[:foods][0..9].map do |data|
       Food.new(data)
     end
   end
