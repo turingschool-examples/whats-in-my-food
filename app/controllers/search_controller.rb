6e36b3706799bccbd071e85543e29709415eee6c
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-05-27 09:26:44 -0600

    feat: add route, controller, view for search#foods

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,8 @@
+class SearchController < ApplicationController
+
+  def foods
+    
+  end
+  
+
+end 
--- spec/features/search/food_search_spec.rb
+++ spec/features/search/food_search_spec.rb
@@ -7,9 +7,10 @@ RSpec.describe 'Food Search' do
 
     it 'When I type in a food and click search I am on the foods page' do
       visit '/'
-      save_and_open_page
-      fill_in 'Ingredient Search', with: 'sweet potatoes'
+
+      fill_in 'q', with: 'sweet potatoes'
       click_on 'Search'
+  
       expect(current_path).to eq('/foods')
 
     end 
