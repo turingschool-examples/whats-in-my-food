2ed929ea3166072ac68a7358469dc5a3977b3f7a
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-05-27 10:54:35 -0600

    Feat: abstract api key to interpolate using ENV variable

--- app/facades/food_facade.rb
+++ app/facades/food_facade.rb
@@ -1,11 +1,7 @@
 class FoodFacade
 
   def self.search_term(term)
-    
-    # conn = Faraday.new(url: "https://api.nal.usda.gov/fdc/v1/foods") 
-    # # response = conn.get("search?api_key=OwdyDMbrhfZf0PNawWLuhbzcljpje6VR8dAp5Jda&query=#{term}")
-    # data = JSON.parse(response.body, symbolize_names: true)[:foods]
-    data = FoodService.search_for_food(term)
+        data = FoodService.search_for_food(term)
     foods = []
     data.each do |food|
       foods << Food.new(food)
--- app/services/food_service.rb
+++ app/services/food_service.rb
@@ -2,7 +2,7 @@ class FoodService
 
   def self.search_for_food(term)
     conn = Faraday.new(url: "https://api.nal.usda.gov/fdc/v1/foods") 
-    response = conn.get("search?api_key=OwdyDMbrhfZf0PNawWLuhbzcljpje6VR8dAp5Jda&query=#{term}")
+    response = conn.get("search?api_key=#{ENV['food_data_key']}&query=#{term}")
     data = JSON.parse(response.body, symbolize_names: true)[:foods]
 
   end 
