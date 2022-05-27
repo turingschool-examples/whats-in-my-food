766e632012fd8f88d1ad55c3071297665c1b7f4e
Sherman Au <shermau13@gmail.com>
2022-05-27 10:54:09 -0600

    test: foods#index

---
+++ spec/features/foods/index_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'foods index' do
+  it 'displays list of foods from search' do
+    visit root_path
+    fill_in :q, with: 'sweet potatoes'
+    click_on 'Search'
+
+    expect(current_path).to eq(foods_path)
+    expect(page).to have_content('Brand Owner', count: 10)
+    expect(page).to have_content('GTIN/UPC Code:', count: 10)
+    expect(page).to have_content('Ingredients:', count: 10)
+    expect(page).to have_content('Search Results')
+    expect(page).to have_content('Total Number of Results:')
+    expect(page).to have_content('Top 10 Matches:')
+  end
+end
