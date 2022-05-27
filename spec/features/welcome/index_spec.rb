99e5a72e800f1484907c7cda7e8d2f13f9d1cdc2
John Hennerich <john.hennerich@gmail.com>
2022-05-27 09:38:43 -0600

    Test: create search tests

---
+++ spec/features/welcome/index_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "Welcome Page" do
+  it "should take user to search results for a food after clicking on button" do
+    visit root_path
+    fill_in :q, with: 'sweet potatoes'
+    click_on('Search')
+    expect(current_path).to eq(foods_path)
+  end
+end
+
+# As a user,
+#  When I visit "/"
+#  And I fill in the search form with "sweet potatoes"
+#  (Note: Use the existing search form)
+#  And I click "Search"
+#  Then I should be on page "/foods"
+#  Then I should see a total of the number of items returned by the search.
+#  (sweet potatoes should find more than 30,000 results)
+#  Then I should see a list of TEN foods that contain the ingredient "sweet potatoes"
+# And for each of the foods I should see:
+#  - The food's GTIN/UPC code
+#  - The food's description
+#  - The food's Brand Owner
+#  - The food's ingredients
