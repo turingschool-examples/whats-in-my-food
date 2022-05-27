e2f3dc6b3e09a09d7ae97b4db96fb5b512e5dd99
Chris Romero <chrisleeromero1@gmail.com>
2022-05-27 09:48:24 -0600

    test: welcome page should allow users to search for food items and return results

---
+++ spec/features/welcome/index_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe 'Welcome page' do
+before(:each) do
+  visit '/'
+  fill_in 'q', with: 'cheese'
+  click_on 'Search'
+end
+  # As a user,
+  #  When I visit "/"
+  #  And I fill in the search form with "sweet potatoes"
+  #  (Note: Use the existing search form)
+  #  And I click "Search"
+  #  Then I should be on page "/foods"
+
+  #  Then I should see a total of the number of items returned by the search.
+  #  (sweet potatoes should find more than 30,000 results)
+
+  #  Then I should see a list of TEN foods that contain the ingredient "sweet potatoes"
+  # And for each of the foods I should see:
+  #  - The food's GTIN/UPC code
+  #  - The food's description
+  #  - The food's Brand Owner
+  #  - The food's ingredients
+  it 'allows the user to search for food items based on ingredient queries' do
+    expect(current_path).to eq('/foods')
+  end
+
+  it 'displays the total results from the query' do
+    expect(page).to have_content('Total results: 54526')
+  end
+
+  it 'displays a list of 10 foods from the results of the query' do
+    expect(page).to have_content('Total results: 54526')
+  end
+
+  it 'displays the food item information from the query' do
+    expect(page).to have_content("039674502084")
+    expect(page).to have_content("CHEESE")
+    expect(page).to have_content("American Pride Food Corp.")
+    expect(page).to have_content("PARMESAN AND ROMANO CHEESE. MADE FROM PASTEURIZED COW'S AND SHEEP'S MILK, CULTURE, SALT, AND ENZYMES. MICROCELLULOSE ADDED TO PREVENT CAKING.")
+  end
+end
