04ed84b23b54e8b6503e7e4ef2448ab852f7178d
Will Medders <wmedders@gmail.com>
2022-05-27 12:14:34 -0400

    FoodDataService #parsed_search_response

--- app/services/food_data_service.rb
+++ app/services/food_data_service.rb
@@ -6,5 +6,8 @@ class FoodDataService
     end
   end
 
-
+  def self.parsed_search_response(query)
+    response = connection.get("search?query=#{query}")
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
--- spec/services/food_data_service_spec.rb
+++ spec/services/food_data_service_spec.rb
@@ -2,8 +2,19 @@ require 'rails_helper'
 
 RSpec.describe 'food data service' do
   describe 'class methods' do
-    it '#conn' do
+    it '#connection' do
       expect(FoodDataService.connection).to be_a(Faraday::Connection)
+    end
+
+    it '#parsed_response' do
+      expect(FoodDataService.parsed_search_response('sweet potatoes')).to be_a(Hash)
+      expect(FoodDataService.parsed_search_response('sweet potatoes')).to have_key(:totalHits)
+      expect(FoodDataService.parsed_search_response('sweet potatoes')).to have_key(:currentPage)
+      expect(FoodDataService.parsed_search_response('sweet potatoes')).to have_key(:totalPages)
+      expect(FoodDataService.parsed_search_response('sweet potatoes')).to have_key(:pageList)
+      expect(FoodDataService.parsed_search_response('sweet potatoes')).to have_key(:foodSearchCriteria)
+      expect(FoodDataService.parsed_search_response('sweet potatoes')).to have_key(:foods)
+      expect(FoodDataService.parsed_search_response('sweet potatoes')).to have_key(:aggregations)      
     end
   end
 end
