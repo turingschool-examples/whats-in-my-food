2a1f15f08c4a2ff948b90b7176c9666187dd6e9a
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-05-27 10:34:04 -0600

    Feat: add usage of food poro to food search controller and view

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,7 +6,11 @@ class SearchController < ApplicationController
     conn = Faraday.new(url: "https://api.nal.usda.gov/fdc/v1/foods") 
     response = conn.get("search?api_key=OwdyDMbrhfZf0PNawWLuhbzcljpje6VR8dAp5Jda&query=#{search_term}")
     data = JSON.parse(response.body, symbolize_names: true)[:foods]
-    @foods = data
+    @foods = []
+    data.each do |food|
+      # binding.pry
+      @foods << Food.new(food)
+    end
   
  end
   
