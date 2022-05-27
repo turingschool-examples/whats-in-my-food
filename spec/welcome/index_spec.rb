be8da2883534cf425c6819d4120412787374a25d
Will Medders <wmedders@gmail.com>
2022-05-27 11:44:25 -0400

    Feature test: search returns a list of foods

--- spec/welcome/index_spec.rb
+++ spec/welcome/index_spec.rb
@@ -9,4 +9,15 @@ RSpec.describe 'welcome page' do
 
     expect(current_path).to eq('/foods')
   end
+
+  it 'search returns a list of foods' do
+    visit '/'
+
+    fill_in :q, with: 'sweet potatoes'
+    click_button 'Search'
+
+    expect(current_path).to eq('/foods')
+
+    expect(page).to have_content('sweet potatoes')
+  end
 end
