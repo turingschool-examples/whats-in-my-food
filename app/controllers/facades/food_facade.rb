b5e73fd152713a0b46039aad6907a572e42da9b3
Sherman Au <shermau13@gmail.com>
2022-05-27 10:54:53 -0600

    add: FoodFacade

---
+++ app/controllers/facades/food_facade.rb
@@ -0,0 +1,15 @@
+class FoodFacade
+  class << self
+    def food_search(food)
+      data = FoodService.find_foods(food)
+      data[:foods].take(10).map do |food_data|
+        Food.new(food_data)
+      end
+    end
+
+    def result_count(food)
+      data = FoodService.find_foods(food)
+      data[:totalHits]
+    end
+  end
+end
