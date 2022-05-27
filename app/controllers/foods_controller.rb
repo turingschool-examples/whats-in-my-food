1a653ef4def5a912ac45bb9bd7011b12d95b7334
Chris Romero <chrisleeromero1@gmail.com>
2022-05-27 09:58:39 -0600

    feat: search button takes user to foods results index page

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,14 @@
+class FoodsController < ApplicationController
+
+  def index
+    connection = Faraday.new(url: "https://api.nal.usda.gov/fdc/v1/") do |f|
+      f.headers["X-API-KEY"] = ENV['food_data_api_key']
+    end
+
+    response = connection.get("foods/search?query=#{params[:q]}")
+
+    data = JSON.parse(response.body, symbolize_names: true)
+
+    # require 'pry'; binding.pry
+  end
+end
