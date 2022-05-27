bc8e352bc818d9981d12f4d0ecb663be9db87883
John Hennerich <john.hennerich@gmail.com>
2022-05-27 10:16:08 -0600

    Feat: connect to api working

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+    results = FoodService.search_for_foods(params[:q])
   end
 end
---
+++ app/services/food_service.rb
@@ -0,0 +1,15 @@
+class FoodService
+  def self.conn
+    Faraday.new(url: 'https://api.nal.usda.gov') do |f|
+      f.params[:api_key] = ENV['govt_api_key']
+    end
+  end
+
+  def self.parse_json(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.search_for_foods(food)
+    conn.get("/fdc/v1/foods/search?#{food}")
+  end
+end
