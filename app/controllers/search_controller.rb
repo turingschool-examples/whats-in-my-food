4f400dec4d9f7f0f840108b94cf799a7e865a689
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-05-27 10:35:43 -0600

    Test: add test for FoodFacade

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,14 +1,13 @@
 class SearchController < ApplicationController
 
   def foods
-    # binding.pry
+  
     search_term = params[:q].gsub(/ /,'%20')
     conn = Faraday.new(url: "https://api.nal.usda.gov/fdc/v1/foods") 
     response = conn.get("search?api_key=OwdyDMbrhfZf0PNawWLuhbzcljpje6VR8dAp5Jda&query=#{search_term}")
     data = JSON.parse(response.body, symbolize_names: true)[:foods]
     @foods = []
     data.each do |food|
-      # binding.pry
       @foods << Food.new(food)
     end
   
---
+++ spec/facades/food_facade_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+
+RSpec.describe FoodFacade do
+
+  it 'returns a food search result' do
+    foods = FoodFacade.search_term
+    expect(foods).to be_a Array
+    expect(foods).to be_all Food
+
+
+  end 
+
+end 
