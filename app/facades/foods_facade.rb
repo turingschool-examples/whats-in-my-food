642c831356b210a67c6e6dc76c042e42db6399d8
Phillip Kamps <phillip.kamps@gmail.com>
2022-05-27 10:59:56 -0600

    Attempt to only show results with ingredients, fail

--- app/facades/foods_facade.rb
+++ app/facades/foods_facade.rb
@@ -6,5 +6,7 @@ class FoodsFacade
 
   def self.food_results(params)
     FoodsService.get_search(params)[:foods][0..9].map { |data| Food.new(data) }
+    # results = FoodsService.get_search(params)
+    # wip = results[:foods].find { |result| result[:ingredients].include?(params) }.limit(10)
   end
 end
