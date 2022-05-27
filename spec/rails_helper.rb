1d29b015bf4a85291f16949481d6738dbc19bf06
Sandisz Thieme <sandiszthieme@gmail.com>
2022-05-27 10:08:10 -0600

    Initialize VCR and SimpleCov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -62,3 +62,11 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+  config.configure_rspec_metadata!
+  config.filter_sensitive_data("Sike you thought") { ENV['edible_api_key'] }
+  config.default_cassette_options = {re_record_interval: 1.day}
+end
--- spec/services/food_service_spec.rb
+++ spec/services/food_service_spec.rb
@@ -3,9 +3,9 @@ require "rails_helper"
 describe "food service" do
   context "class methods" do
     context "#search" do
-      it "returns foods related to search results" do
+      it "returns foods related to search results", :vcr do
         search_response = FoodService.search("cheese")
-
+        binding.pry
         expect(search_response).to be_a Hash
         expect(search_response[:foods]).to be_an Array
         expect(search_response[:foods].first).to be_a Hash
