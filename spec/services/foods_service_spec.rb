b8ef9699cb55b2529b52f8eca6801181a159394e
Scott Sullivan <scottsullivanltd@gmail.com>
2022-05-29 17:48:17 -0600

    One last refactor for syntax change - does not affect functionality or tests

--- spec/services/foods_service_spec.rb
+++ spec/services/foods_service_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe FoodsService do
-  describe "API repo endpoint" do
+  describe "API food endpoint" do
     it "gets food data from endpoint", :vcr do
       json = FoodsService.get_food_data("sweet potatoes")
       expect(json[:foodSearchCriteria][:query]).to eq("sweet potatoes")
