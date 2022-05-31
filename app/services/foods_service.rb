997bea04a7ba2a752abb88210201bffc758ddf7a
Oscar Santos <oscar303data@gmail.com>
2022-05-30 19:52:15 -0600

    - Refactor foods services in order to get the branded names of foods.

--- app/services/foods_service.rb
+++ app/services/foods_service.rb
@@ -1,10 +1,11 @@
 class FoodsService
   def self.getting_list(query)
-    list = '/fdc/v1/foods/search/'
+    list = '/fdc/v1/foods/search'
 
     response = connection.get(list) do |faraday|
       faraday.params["api_key"] = ENV['API_KEY']
-      faraday.params["query"] = query
+      faraday.params["search"] = query
+      faraday.params["dataType"] = 'Branded'
     end
      JSON.parse(response.body, symbolize_names: true)
   end
