6ac4a48d30519cd4b93d9d2ab479a92d48e5adb6
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2022-09-09 09:50:14 -0600

    facade complete

--- app/facades/food_facade.rb
+++ app/facades/food_facade.rb
@@ -2,8 +2,7 @@ class FoodFacade
     
     def self.search_list_by_food(food)
         parsed_json = FoodService.search_food(food)
-        parsed_json[:foods].map do |food_json|
-            require 'pry'; binding.pry 
+        parsed_json[:foods].map do |food_json| 
           Food.new(food_json)
         end
     end
