225cd883ee01d4708fa518da7bcb6dcd34bb34cb
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2022-09-09 09:18:59 -0600

    Add spec folders

---
+++ spec/features/foods/index_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe 'Foods Index Page' do
+  it 'shows the total number of items returned by the search' do
+
+  end
+
+  it 'shows the ten foods that contain the ingredient searched for' do
+
+  end
+
+  it "shows the food's GTIN/UPC code" do
+
+  end
+
+  it "shows the food's description" do
+
+  end
+
+  it "shows the food's Brand Owner" do
+
+  end
+
+  it "shows the food's ingredients" do
+    
+  end
+end
---
+++ spec/features/welcome/index_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe 'Welcome Index Page' do
+  describe 'search form' do
+    it 'can search for foods' do
+
+    end
+
+    it 'redirects to foods index page' do
+
+    end
+  end
+end
