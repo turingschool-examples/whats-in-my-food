37d6b5960ca72683424278565460b8b9b8880c55
Bryce Simonds <bsimonds14@gmail.com>
2022-09-09 09:19:35 -0600

    Feat: search button from landing page brings you to /foods

--- spec/features/landing_spec.rb
+++ spec/features/landing_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 RSpec.describe 'Landing Page' do 
   it 'has a search button that when clicked brings you to /foods' do 
-    expect(page).to have_content('Search')
+    visit '/'
     
-    click_on 'Search'
+    expect(page).to have_button('Search')
+    
+    click_button 'Search'
 
     expect(current_path).to eq('/foods')
   end
