0a29aa86e705b116beee9ee9200b694127234056
Jonathan Pope <jonathanmpope@gmail.com>
2022-09-09 09:17:55 -0600

    feat: user search path with test

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'Food Search' do
         it 'allows a user to search for foods' do
             visit '/'
             
-            fill_in "search", with: "Toy Story"
+            fill_in "q", with: "sweet potatoes"
             click_button("Search")
 
             expect(current_path).to eq("/foods")
