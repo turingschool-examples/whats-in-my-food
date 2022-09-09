da1054638d793b4e680f2a759430f70fccf0677c
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2022-09-09 09:33:30 -0600

    Add test conditions to welcome index spec

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -6,14 +6,19 @@ RSpec.describe 'Welcome Index Page' do
   end
 
   it "has a search form" do
-
+    expect(page).to have_field(:search)
+    expect(page).to have_button("Search")
   end
 
   it "can search for foods" do
-
+    fill_in :search, with: "sweet potatoes"
+    click_button "Search"
   end
 
-  it "redirects to foods index page" do
+  it "redirects to foods index page after search" do
+    fill_in :search, with: "sweet potatoes"
+    click_button "Search"
 
+    expect(current_path).to eq(foods_path)
   end
 end
