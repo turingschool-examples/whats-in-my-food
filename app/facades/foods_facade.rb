30395eef9a68846a16c5ca3165880f3285bf4e60
Shirley DeCesari <shirley.decesari@gmail.com>
2022-09-09 09:28:48 -0700

    Finish facade and service

--- app/facades/foods_facade.rb
+++ app/facades/foods_facade.rb
@@ -1,8 +1,13 @@
 class FoodsFacade
   def self.get_list(q)
     json = FoodsService.get_list_of_foods(q)
-    json[:foods].map do |food|
-      binding.pry
+    json[:foods][0..9].map do |food|
+      # binding.pry
+      Food.new(food)
     end 
+  end
+
+  def self.food_count(q)
+    FoodService.get_list_of_foods(q)[:totalHits]
   end
 end 
