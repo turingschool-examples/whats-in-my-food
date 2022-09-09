6b7691cf6e73c3b8e7ce82103197276977b0e0cf
Eli Sachs <easachs13@gmail.com>
2022-09-09 10:38:46 -0600

    Refactor food poro

--- app/poros/food.rb
+++ app/poros/food.rb
@@ -1,9 +1,13 @@
 class Food
   attr_reader :code, :description, :brand, :ingredients
   def initialize(data)
-    @code = data[:gtinUpc] != ''? data[:gtinUpc] : 'No code found.'
-    @description = data[:description] != ''? data[:description] : 'No description found.'
-    @brand = data[:brandOwner] != ''? data[:brandOwner] : 'No brand found.'
-    @ingredients = data[:ingredients] != ''? data[:ingredients] : 'No ingredients found.'
+    @code = nil_check(data[:gtinUpc]) ? 'No code found.' : data[:gtinUpc]
+    @description = nil_check(data[:description]) ? 'No description found.' : data[:description]
+    @brand = nil_check(data[:brandOwner]) ? 'No brand found.' : data[:brandOwner]
+    @ingredients = nil_check(data[:ingredients]) ? 'No ingredients found.' : data[:ingredients]
+  end
+
+  def nil_check(attribute)
+    ['', nil].include?(attribute)
   end
 end
