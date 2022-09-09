b3b17a80b96e8276e20106d9a2ee770a4dd599be
Bryce Simonds <bsimonds14@gmail.com>
2022-09-09 10:05:53 -0600

    Feat: food db service retrieves data for searched food

--- spec/services/food_db_service_spec.rb
+++ spec/services/food_db_service_spec.rb
@@ -9,10 +9,10 @@ RSpec.describe 'Food DB Service' do
 
     first_food_object = parsed_json[:foods][0]
     expect(first_food_object).to include(:description, :gtinUpc, :brandOwner, :ingredients)
-    expect(first_food_object.description).to be_a String
-    expect(first_food_object.gtinUpc).to be_a String
-    expect(first_food_object.brandOwner).to be_a String
-    expect(first_food_object.ingredients).to be_a String
+    expect(first_food_object[:description]).to be_a String
+    expect(first_food_object[:gtinUpc]).to be_a String
+    expect(first_food_object[:brandOwner]).to be_a String
+    expect(first_food_object[:ingredients]).to be_a String
   end
 end
 
