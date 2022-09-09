58f7d432136644b1b0d15974fe1c75ae6901db2b
Nick Jones <nickjones9605@gmail.com>
2022-09-09 10:07:16 -0600

    Fixes typo

--- spec/services/food_service_spec.rb
+++ spec/services/food_service_spec.rb
@@ -2,8 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'FoodService', :vcr do
     it "returns Food data" do
-        foods = FoodService.get_foods
+        food = FoodService.get_foods("sweet potatoes")
 
-        expect(foods).to be_a(Hash)
+        expect(food).to be_a(Hash)
+        expect(food).to have_key(:totalHits)
+        expect(food[:totalHits]).to be_a(Integer)
+
+        expect(food).to have_key(:foods)
+        expect(food[:foods]).to be_a(Array)
     end
 end
