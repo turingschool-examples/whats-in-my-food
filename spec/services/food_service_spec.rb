1e1ecdde2cdbd421d00e581f6c790383daadc217
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2022-09-09 09:21:27 -0600

    services working

--- spec/services/food_service_spec.rb
+++ spec/services/food_service_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe FoodService do
     it 'gets data from api' do
-        foods = FoodService.search_food('cheese')
+        foods = FoodService.search_food('cheese') 
         expect(foods).to be_a(Hash)      
     end
 end
