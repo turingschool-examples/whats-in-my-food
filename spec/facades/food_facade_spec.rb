8b8b95d4e0d8ab26ba9345ba4b6ea14d9f374e8f
Ethan Nguyen <etnguyen16@gmail.com>
2022-09-09 10:13:57 -0600

    add without working cassettes

---
+++ spec/facades/food_facade_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe FoodsFacade, :vcr do
+  describe "search results" do
+    it "returns foods that match the search keyword" do
+      foods = FoodsFacade.search_by_keyword("sweet potatoes")
+
+      expect(foods).to be_an(Array)
+      expect(foods.first).to be_an_instance_of(Food)
+      expect(foods.last).to be_an_instance_of(Food)
+    end
+  end
+end
