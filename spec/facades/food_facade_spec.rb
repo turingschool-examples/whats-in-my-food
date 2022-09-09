0a61846fbc1f974e467816c57888f11d350f5b8c
Bryce Simonds <bsimonds14@gmail.com>
2022-09-09 10:53:19 -0600

    Test: returns a top hits object

--- spec/facades/food_facade_spec.rb
+++ spec/facades/food_facade_spec.rb
@@ -7,6 +7,10 @@ RSpec.describe 'Food Facade' do
     expect(array_of_food_objects).to be_a Array
     expect(array_of_food_objects).to be_all Food
   end
+
+  it 'returns a top hits object' do 
+    expect(FoodDBFacade.the_total_hits_for_search("sweet potatoes")).to be_a FoodTotalHits
+  end
 end
 
 
