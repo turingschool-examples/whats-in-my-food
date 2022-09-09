04728cfcecb74bc6bcfcc995096a09d454b2b29b
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2022-09-09 09:59:43 -0600

    built out poro

--- app/poros/food.rb
+++ app/poros/food.rb
@@ -1,7 +1,10 @@
 class Food
-    attr_reader :name
+    attr_reader :name, :code, :brand, :ingredients
 
     def initialize(food_data)
         @name = food_data[:description]
+        @code = food_data[:fdcId]
+        @brand = food_data[:brandOwner]
+        @ingredients = food_data[:ingredients]
     end
 end
