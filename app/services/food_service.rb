9d42e265613d51564bb272ae786e6f298d8fa206
Nick Jones <nickjones9605@gmail.com>
2022-09-09 10:19:15 -0600

    adds vcr cassette for testing

--- app/services/food_service.rb
+++ app/services/food_service.rb
@@ -3,7 +3,7 @@ class FoodService
         conn = Faraday.new(url: 'https://api.nal.usda.gov/fdc/v1/') do |faraday|
             faraday.headers['api_key'] = ENV['food_api_key']
         end
-        response = conn.get("foods/search?api_key=#{ENV['food_api_key']}&query=#{food}&pageSize=10&pageNumber=1")
+        response = conn.get("foods/search?api_key=#{ENV['food_api_key']}&query=#{food}")
         body = JSON.parse(response.body, symbolize_names: true)
     end
 end
--- spec/services/food_service_spec.rb
+++ spec/services/food_service_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'FoodService', :vcr do
     it "returns Food data" do
-        food = FoodService.get_foods("sweet potatoes")
+        food = FoodService.get_foods("cheddar cheese")
 
         expect(food).to be_a(Hash)
         expect(food).to have_key(:totalHits)
@@ -10,5 +10,19 @@ RSpec.describe 'FoodService', :vcr do
 
         expect(food).to have_key(:foods)
         expect(food[:foods]).to be_a(Array)
+
+        first_food = food[:foods].first
+
+        expect(first_food).to have_key(:description)
+        expect(first_food[:description]).to be_a(String)
+
+        expect(first_food).to have_key(:gtinUpc)
+        expect(first_food[:gtinUpc]).to be_a(String)
+
+        expect(first_food).to have_key(:brandOwner)
+        expect(first_food[:brandOwner]).to be_a(String)
+
+        expect(first_food).to have_key(:ingredients)
+        expect(first_food[:ingredients]).to be_a(String)
     end
 end
