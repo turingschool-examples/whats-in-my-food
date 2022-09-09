6735e705e2c19b21f1a5668f23051670e35e0115
Shirley DeCesari <shirley.decesari@gmail.com>
2022-09-09 09:47:00 -0700

    Add poros

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,6 +1,6 @@
 class FoodsController < ApplicationController
   def index
     @foods = FoodsFacade.get_list(params[:q])  
-    @food_count = FoodFacade.food_count(params[:q])
+    @food_count = FoodsFacade.food_count(params[:q])
   end
 end 
--- app/facades/foods_facade.rb
+++ app/facades/foods_facade.rb
@@ -2,12 +2,11 @@ class FoodsFacade
   def self.get_list(q)
     json = FoodsService.get_list_of_foods(q)
     json[:foods][0..9].map do |food|
-      # binding.pry
       Food.new(food)
     end 
   end
 
   def self.food_count(q)
-    FoodService.get_list_of_foods(q)[:totalHits]
+    FoodsService.get_list_of_foods(q)[:totalHits]
   end
 end 
