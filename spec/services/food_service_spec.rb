3168d9b8d7c77660833b8bb0a85bdddcade696ec
Nicole Esquer <nicole.esquer5@gmail.com>
2022-09-09 09:56:51 -0700

    testing: service testing complete

--- spec/services/food_service_spec.rb
+++ spec/services/food_service_spec.rb
@@ -1,7 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe FoodService do
-   xit 'can pull data about food' do
+   it 'can pull data about food' do
       results = FoodService.new
+      food_results = results.food_search("sweet potatoes")
+
+      expect(food_results).to be_a(Hash)
+
+      expect(food_results).to have_key(:foods)
+      expect(food_results[:foods]).to be_a(Array)
+
+      first_result = food_results[:foods].first
+
+      expect(first_result).to have_key(:brandOwner)
+      expect(first_result).to have_key(:description)
+      expect(first_result).to have_key(:gtinUpc)
+      expect(first_result).to have_key(:ingredients)
    end
 end
