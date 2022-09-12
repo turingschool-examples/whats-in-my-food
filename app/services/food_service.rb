1e51b78f75414e0a10451c77777769b24f14c3eb
Sunny Moore <moore.sunny@gmail.com>
2022-09-12 07:36:41 -0700

    fixed url in service so test would pass

--- app/services/food_service.rb
+++ app/services/food_service.rb
@@ -1,14 +1,14 @@
 class FoodService
 
   def self.search_food_by_keyword(keyword)
-    response = conn.get("/foods/search?query=#{keyword}&pageSize=10&pageNumber=1&sortBy=dataType.keyword&sortOrder=asc")
+    response = conn.get("/fdc/v1/foods/search?query=#{keyword}&pageSize=10&pageNumber=1&sortBy=dataType.keyword&sortOrder=asc&api_key=#{ENV['food_api_key']}")
     JSON.parse(response.body, symbolize_names: true)
   end
 
   private
 
   def self.conn
-    Faraday.new(url: "https://api.nal.usda.gov/fdc/v1") do |faraday|
+    Faraday.new(url: "https://api.nal.usda.gov") do |faraday|
       faraday.headers["X-API-KEY"] = ENV['food_api_key']
     end
   end
--- spec/service/food_service_spec.rb
+++ spec/service/food_service_spec.rb
@@ -5,13 +5,13 @@ RSpec.describe 'Food Service' do
     parsed_json = FoodService.search_food_by_keyword('sweet potatoes')
 
     expect(parsed_json).to be_a Hash
-    
     expect(parsed_json[:foods]).to be_a Array
 
     food = parsed_json[:foods].first
-    expect(food).to include :description, :gtinUPC, :brandOwner, :ingredients
+    # binding.pry
+    expect(food).to include :description, :ingredients, :brandOwner, :gtinUpc
     expect(food[:description]).to be_a String
-    expect(food[:gtinUPC]).to be_a String
+    expect(food[:gtinUpc]).to be_a String
     expect(food[:brandOwner]).to be_a String
     expect(food[:ingredients]).to be_a String
   end
